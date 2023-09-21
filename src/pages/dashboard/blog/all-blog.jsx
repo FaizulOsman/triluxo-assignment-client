a97dad4a1cb3450061c74c42e398cd9892f52e84
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-21 22:19:48 +0600

    feat: all blog show in dashboard

--- src/pages/dashboard/blog/all-blog.jsx
+++ src/pages/dashboard/blog/all-blog.jsx
@@ -15,7 +15,6 @@ import { MdDeleteOutline } from "react-icons/md";
 const jwt = require("jsonwebtoken");
 
 const AllBlog = () => {
-  const [blogCategory, setBlogCategory] = useState("English");
   const { data: allBlog } = useGetAllBlogQuery();
 
   const accessToken =
@@ -42,10 +41,6 @@ const AllBlog = () => {
     } else {
       blogUniqueSubjects.push(blog);
     }
-  });
-
-  const filterByBlogSubject = allBlog?.data?.filter((blog) => {
-    return blog?.subject === blogCategory;
   });
 
   const [
@@ -102,83 +97,60 @@ const AllBlog = () => {
     <div>
       <div className="my-20 w-11/12 md:w-10/12 mx-auto">
         <h1 className="text-3xl font-semibold text-center my-8">Blogs</h1>
-        {blogUniqueSubjects && blogUniqueSubjects.length > 0 ? (
-          <div className="w-10/12 md:w-8/12 mx-auto flex flex-wrap justify-around gap-3">
-            {blogUniqueSubjects?.map((blog, index) => (
+        {allBlog?.data ? (
+          <div className="mt-10 flex flex-col gap-5">
+            {allBlog?.data?.map((blog, index) => (
               <div
                 key={index}
-                onClick={() => setBlogCategory(blog?.subject)}
-                className={`relative mb-2 min-w-[96px] text-center hover:bg-green-500 px-2 py-1 rounded-md cursor-pointer hover:text-white font-semibold ${
-                  blogCategory === blog?.subject
-                    ? "bg-green-500"
-                    : " bg-[#1d1836]"
-                }`}
+                className="flex justify-between items-center bg-[#1d1836] p-2 rounded-md"
               >
-                {blogCategory === blog?.subject && (
-                  <div className="absolute top-full left-[45%] triangle_down border-l-[7px] border-r-[7px] border-t-[10px] border-t-green-500 border-transparent"></div>
-                )}
-                {blog?.subject}
+                <div className="w-[70%] pr-2">
+                  <p className="text-md">Title: {blog?.title}</p>
+                  <p className="text-sm mt-2">
+                    Description: {blog?.description.slice(0, 200)}
+                  </p>
+                </div>
+                <div className="flex flex-col items-center justify-between gap-4">
+                  <div className="flex items-center justify-between gap-4">
+                    <button
+                      onClick={() => handleDeleteBlog(blog)}
+                      className="text-2xl border-none  text-red-500 hover:text-red-600"
+                    >
+                      <MdDeleteOutline />
+                    </button>
+                    <Link
+                      href={`/dashboard/blog/update/${blog?.id}`}
+                      className="flex items-center"
+                    >
+                      <button className="text-lg border-none text-primary hover:text-blue-600">
+                        <FaRegEdit />
+                      </button>
+                    </Link>
+                  </div>
+                  {blog?.isPublished ? (
+                    <button
+                      onClick={() => handlePublish(blog, false)}
+                      className="w-full h-full btn-xs border-2 border-blue-500 rounded-md px-2 text-blue-500 hover:bg-blue-500 hover:text-white"
+                    >
+                      Remove Publish
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handlePublish(blog, true)}
+                      className="w-full h-full btn-xs border-2 border-blue-500 rounded-md px-2 text-blue-500 hover:bg-blue-500 hover:text-white"
+                    >
+                      Make Publish
+                    </button>
+                  )}
+                </div>
               </div>
             ))}
           </div>
         ) : (
-          <Loader />
+          <div className="py-10">
+            <Loader />
+          </div>
         )}
-        <div className="mt-10 flex flex-col gap-5">
-          {filterByBlogSubject?.map((blog, index) => (
-            <div
-              key={index}
-              className="flex justify-between items-center bg-[#1d1836] p-2 rounded-md"
-            >
-              <div>
-                <h4 className="text-md font-semibold">
-                  {blog?.subject} {blog?.serial}
-                </h4>
-                <p>Question: {blog?.questions?.length}</p>
-                <p>Time: {blog?.timeLimit} min</p>
-              </div>
-              <div className="flex flex-col items-center justify-between gap-4">
-                <div className="flex items-center justify-between gap-4">
-                  <button
-                    onClick={() => handleDeleteBlog(blog)}
-                    className="text-2xl border-none  text-red-500 hover:text-red-600"
-                  >
-                    <MdDeleteOutline />
-                  </button>
-                  <Link
-                    href={`/dashboard/blog/update/${blog?.id}`}
-                    className="flex items-center"
-                  >
-                    <button className="text-lg border-none text-primary hover:text-blue-600">
-                      <FaRegEdit />
-                    </button>
-                  </Link>
-                </div>
-                <Link
-                  href={`/dashboard/blog-result/${blog?.id}`}
-                  className="border-2 border-blue-500 rounded-md px-2 text-blue-500 hover:bg-blue-500 hover:text-white"
-                >
-                  Results
-                </Link>
-                {blog?.isPublished ? (
-                  <button
-                    onClick={() => handlePublish(blog, false)}
-                    className="btn-xs border-2 border-blue-500 rounded-md px-2 text-blue-500 hover:bg-blue-500 hover:text-white"
-                  >
-                    Remove Publish
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => handlePublish(blog, true)}
-                    className="btn-xs border-2 border-blue-500 rounded-md px-2 text-blue-500 hover:bg-blue-500 hover:text-white"
-                  >
-                    Make Publish
-                  </button>
-                )}
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
     </div>
   );
